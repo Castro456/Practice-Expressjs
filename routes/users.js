142632b7544fa724f1c2a395aba245a1e8a624bb
Castro456 <castrosid456@gmail.com>
2025-01-23 03:33:28 +0530

    Passing vales to routes

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,9 @@ router.get("/new", (req, res) => {
   res.status(200).send('Create user coming soon')
 })
 
+router.get("/:id", (req, res) => { // this route will accept the the values also, so always write this routes below
+  // req.params.id // should be the same as above
+  res.status(200).send(`The user id is ${req.params.id}`)
+})
+
 module.exports = router
