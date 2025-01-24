96426d01a47dcb6d6edcb1b8714f56ed3de0e4a4
Castro456 <castrosid456@gmail.com>
2025-01-24 15:39:27 +0530

    Static pages middleware

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express()
 app.set('view engine', 'ejs') //static code
 // app.use(logger)
 
-app.get('/', (req, res) => {
+// This middleware is used for calling static pages like html, css, js.
+// Declare the folder name and access every files inside that folder.
+app.use(express.static("public"))
+
+app.get('/hello', (req, res) => {
   console.log('hello get method');
   // res.status(200).json({"message": "Hello for Express.js"}) //chain the status
   // res.download("server.js") //This will directly download the file 
